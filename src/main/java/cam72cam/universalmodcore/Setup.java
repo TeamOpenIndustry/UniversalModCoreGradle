bc9eb25ce7a2f8fb33b3eb2eeaa86f89b112e7f1
Christian Mesh <christianmesh1@gmail.com>
2020-03-31 08:25:08 -0400

    Add Util and fix dependencies

--- src/main/java/cam72cam/universalmodcore/Setup.java
+++ src/main/java/cam72cam/universalmodcore/Setup.java
@@ -1,12 +1,14 @@
 package cam72cam.universalmodcore;
 
 import org.gradle.api.DefaultTask;
+import org.gradle.api.Task;
 import org.gradle.api.tasks.TaskAction;
 
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Set;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
---
+++ src/main/java/cam72cam/universalmodcore/Util.java
@@ -0,0 +1,57 @@
+package cam72cam.universalmodcore;
+
+import org.apache.commons.io.FileUtils;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Util {
+    public static void GitClone(String repository, String branch, String path) throws IOException, GitAPIException {
+        File clonePath = new File(".", path);
+        String useSSH = System.getProperty("ssh.http");
+        if (useSSH != null) {
+            useSSH = useSSH.toLowerCase();
+        }
+
+        String uri = repository;
+
+        boolean wantsHttp = "yes".equals(useSSH) || "true".equals(useSSH);
+        boolean wantsGit = "no".equals(useSSH) || "false".equals(useSSH);
+        boolean isHttp = uri.startsWith("http");
+        boolean isGit = uri.startsWith("git@");
+
+        if (isHttp && wantsGit) {
+            uri = uri.replaceFirst("https://", "git@");
+            uri = uri.replaceFirst("http://", "git@");
+            uri = uri.replaceFirst("/", ":");
+        }
+        if (!isGit && wantsHttp) {
+            uri = uri.replaceFirst(":", "/");
+            uri = uri.replaceFirst("git@", "https://");
+        }
+
+        if (clonePath.exists()) {
+            System.out.println("Removing " + clonePath);
+            FileUtils.deleteDirectory(clonePath);
+        }
+
+        clonePath.mkdirs();
+
+        System.out.println("Cloning " + uri + " into " + clonePath);
+
+
+        Git.cloneRepository()
+                .setDirectory(clonePath)
+                .setURI(uri)
+                .setNoCheckout(true)
+                .setCloneAllBranches(true)
+                .call()
+                .checkout()
+                .setCreateBranch(true)
+                .setName(branch)
+                .setStartPoint("origin/" + branch)
+                .call();
+    }
+}
